cd1bafab3ae3f5e621b799f5fd6190b61f322307
missym <missym@parrot>
2017-08-16 20:51:01 +0100

    duvida

--- src/connection/DbConn.java
+++ src/connection/DbConn.java
@@ -5,125 +5,138 @@
  */
 package connection;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.sql.*;
+import java.util.ArrayList;
+
 /**
  *
  * @author missym
  */
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.Date;
-
 public class DbConn {
-    private Connection connect = null;
-    private Statement statement = null;
-    private PreparedStatement preparedStatement = null;
-    private ResultSet resultSet = null;
 
-    public void readDataBase() throws Exception {
-        try {
-            // This will load the MySQL driver, each DB has its own driver
-            Class.forName("com.mysql.jdbc.Driver");
-            // Setup the connection with the DB
-            connect = DriverManager
-                    .getConnection("jdbc:mysql://localhost/feedback?"
-                            + "user=missym&password=456");
-
-            // Statements allow to issue SQL queries to the database
-            statement = connect.createStatement();
-            // Result set get the result of the SQL query
-            resultSet = statement
-                    .executeQuery("select * from feedback.comments");
-            writeResultSet(resultSet);
-
-            // PreparedStatements can use variables and are more efficient
-            preparedStatement = connect
-                    .prepareStatement("insert into  feedback.comments values (default, ?, ?, ?, ? , ?, ?)");
-            // "myuser, webpage, datum, summary, COMMENTS from feedback.comments");
-            // Parameters start with 1
-            preparedStatement.setString(1, "Test");
-            preparedStatement.setString(2, "TestEmail");
-            preparedStatement.setString(3, "TestWebpage");
-            preparedStatement.setDate(4, new java.sql.Date(2009, 12, 11));
-            preparedStatement.setString(5, "TestSummary");
-            preparedStatement.setString(6, "TestComment");
-            preparedStatement.executeUpdate();
-
-            preparedStatement = connect
-                    .prepareStatement("SELECT myuser, webpage, datum, summary, COMMENTS from feedback.comments");
-            resultSet = preparedStatement.executeQuery();
-            writeResultSet(resultSet);
-
-            // Remove again the insert comment
-            preparedStatement = connect
-            .prepareStatement("delete from feedback.comments where myuser= ? ; ");
-            preparedStatement.setString(1, "Test");
-            preparedStatement.executeUpdate();
-
-            resultSet = statement
-            .executeQuery("select * from feedback.comments");
-            writeMetaData(resultSet);
-
-        } catch (Exception e) {
-            throw e;
-        } finally {
-            close();
-        }
-
-    }
-
-    private void writeMetaData(ResultSet resultSet) throws SQLException {
-        //  Now get some metadata from the database
-        // Result set get the result of the SQL query
-
-        System.out.println("The columns in the table are: ");
-
-        System.out.println("Table: " + resultSet.getMetaData().getTableName(1));
-        for  (int i = 1; i<= resultSet.getMetaData().getColumnCount(); i++){
-            System.out.println("Column " +i  + " "+ resultSet.getMetaData().getColumnName(i));
-        }
-    }
-
-    private void writeResultSet(ResultSet resultSet) throws SQLException {
-        // ResultSet is initially before the first data set
-        while (resultSet.next()) {
-            // It is possible to get the columns via name
-            // also possible to get the columns via the column number
-            // which starts at 1
-            // e.g. resultSet.getSTring(2);
-            String user = resultSet.getString("myuser");
-            String website = resultSet.getString("webpage");
-            String summary = resultSet.getString("summary");
-            Date date = resultSet.getDate("datum");
-            String comment = resultSet.getString("comments");
-            System.out.println("User: " + user);
-            System.out.println("Website: " + website);
-            System.out.println("summary: " + summary);
-            System.out.println("Date: " + date);
-            System.out.println("Comment: " + comment);
-        }
-    }
-
-    // You need to close the resultSet
-    private void close() {
-        try {
-            if (resultSet != null) {
-                resultSet.close();
-            }
-
-            if (statement != null) {
-                statement.close();
-            }
-
-            if (connect != null) {
-                connect.close();
-            }
-        } catch (SQLException e) {
-
-        }
-    }
-
+	static String user = "missym";
+	static String pass = "456";
+	static String db   = "AsyioDB";
+	static String url  = "jdbc:mysql://localhost:3306/"+db;
+	
+	static Connection con;
+	static Statement st ;
+	static ResultSet rs ;
+	static PreparedStatement prep;
+	
+	public DbConn() {
+		
+	}
+	
+	
+	public Connection getConnection() {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			con = DriverManager.getConnection(url,user,pass);
+			return con;
+		} catch (ClassNotFoundException | SQLException e) {
+		}
+		return null;	
+	}
+	
+	public void deleteFrom(Object obj) throws NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException{
+		Method temp=obj.getClass().getMethod("delete");
+		String s=(String) temp.invoke(obj);
+		try {
+			prep = getConnection().prepareStatement(s);			
+			prep.execute();
+			con.close();
+		} catch (SQLException e) {
+		}
+	}
+	
+	public void insertInto(Object obj) throws NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException{
+		Method temp=obj.getClass().getMethod("insert");
+		String s=(String) temp.invoke(obj);
+		try {
+			prep = getConnection().prepareStatement(s);			
+			prep.execute();
+			con.close();
+		} catch (SQLException e) {
+		}
+		
+	}
+	
+	public void updateWhere(Object obj) throws NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException{
+		Method temp=obj.getClass().getMethod("update");
+		String s=(String) temp.invoke(obj);
+		try {
+			prep = getConnection().prepareStatement(s);			
+			prep.execute();
+			con.close();
+		} catch (SQLException e) {
+		}
+		
+	}
+	
+	public ArrayList<Object> getAll(Object o) throws IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException, SecurityException {
+		Method temp=o.getClass().getMethod("select");
+		String s=(String) temp.invoke(o);
+		Method temp1=o.getClass().getMethod("sqlObjectContructor");		
+		ArrayList<Object> all = new ArrayList<>();
+		try {
+			prep = getConnection().prepareStatement(s);
+			rs = prep.executeQuery();
+			while(rs.next()) {
+				
+				all.add(temp1.invoke(o, rs));
+			}
+			con.close();
+			return all;
+		} catch (SQLException e) {
+		}
+		
+		return null;
+	}
+	
+	public Object selectOneById(Object obj) throws NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException{
+		Method temp=obj.getClass().getMethod("getId");
+		int i=(int) temp.invoke(obj);		
+		String x="SELECT * FROM "+obj.getClass().getSimpleName()+" WHERE id="+Integer.toString(i)+";";
+		Method temp1=obj.getClass().getMethod("sqlObjectContructor");		
+				
+		try {
+			prep = getConnection().prepareStatement(x);			
+			rs = prep.executeQuery();
+			Object o=temp1.invoke(obj, rs);
+			con.close();
+			return o;
+		} catch (SQLException e) {
+		}
+		
+		return null;		
+	} 
+	
+	public ArrayList<Object> selectFillArrayInObject(Object obj, Object arr) throws NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException{
+		String clas=""+arr.getClass().getSimpleName()+"";
+		Method temp=obj.getClass().getMethod("getId");
+		int i=(int) temp.invoke(obj);
+		Method temp0=obj.getClass().getMethod("liga");
+		String liga=(String) temp0.invoke(obj);
+		//select * from music left join liga on liga.music=music.id WHERE liga.album= bla.getid();
+		String x="SELECT * FROM "+clas+" LEFT JOIN "+liga+" ON "+liga+"."+clas+" = "+clas+".id  WHERE "+liga+"."+obj.getClass().getSimpleName()+"="+Integer.toString(i)+";";
+		
+		Method temp1=arr.getClass().getMethod("sqlObjectContructor");		
+		ArrayList<Object> all = new ArrayList<>();		
+		try {
+			prep = getConnection().prepareStatement(x);			
+			rs = prep.executeQuery();
+			while(rs.next()) {
+				
+				all.add(temp1.invoke(arr, rs));
+			}
+			con.close();
+			return all;
+		} catch (SQLException e) {
+		}		
+		return null;		
+	}
+	
 }
